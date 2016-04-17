e9c8dd2fb8ffd5b0a23b46429355df8cba75ebbc
sb8389 <deals8389@gmail.com>
2016-04-17 11:55:56 -0400

    fixing typos

--- UserCtrl.js
+++ UserCtrl.js
@@ -1,13 +1,13 @@
 (function(){
   'use strict';
-    var app=angular.module("userApp",[]) ;
-    app.controller("userCtrl",function($scope) {
-      $scope.vaidUser={};
-      $scope.login= function(){
-        if($scope.userName==="sb" && $scope.password==="sb123"){
-            $scope.validUser.name=$scope.userName;
-        }else {
-            $scope.validUser = {};
+    var app = angular.module("userApp", []) ;
+    app.controller("userCtrl", function ($scope) {
+      $scope.validUser = { 'name' : '' };
+      $scope.login = function (){
+        if($scope.userName === "sb" && $scope.password === "sb123"){
+            $scope.validUser.name = $scope.userName;
+        }else{
+            $scope.validUser = { 'name' : '' };
         }
       }
    });
