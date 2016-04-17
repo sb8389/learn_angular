bf648ae64ee4854b654a5a17077410d01c7ee129
Rajesh Kanakabandi <rajesh.kanakabandi@gmail.com>
2016-04-17 12:32:56 -0400

    small refinements

--- UserCtrl.js
+++ UserCtrl.js
@@ -2,13 +2,18 @@
   'use strict';
     var app = angular.module("userApp", []) ;
     app.controller("userCtrl", function ($scope) {
+      $scope.loginError = null;
       $scope.validUser = { 'name' : '' };
       $scope.login = function (){
         if($scope.userName === "sb" && $scope.password === "sb123"){
             $scope.validUser.name = $scope.userName;
+            $scope.loginError = null;
         }else{
-            $scope.validUser = { 'name' : '' };
+            $scope.loginError = "Invalid username and password combination";
         }
+      }
+      $scope.logout = function(){
+          $scope.validUser.name = '';
       }
    });
 
